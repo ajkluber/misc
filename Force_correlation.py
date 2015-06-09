Calculate Hessian corrected

Force_correlation.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 import time
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 import model_builder as mdb
 
@@ -101,6 +101,15 @@ if __name__ == "__main__":
     egvals_TS, rotation_TS = np.linalg.eig(HessianTS)
     egvals_N,  rotation_N = np.linalg.eig(HessianN)
 
+    if not os.path.exists("Hessian"):
+        os.mkdir("Hessian")
+    os.chdir("Hessian")
+
+    np.save("Hessian_U.npy",HessianU)
+    np.save("Hessian_TS.npy",HessianTS)
+    np.save("Hessian_N.npy",HessianN)
+
+    os.chdir("../../..")
 
     """
     normal_U = np.dot(Forceij_U,rotation_U)

cumulant_exp.py
@@ -2,6 +2,8 @@
 import os
 import argparse
 import numpy as np
+import matplotlib 
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from scipy.special import gamma
@@ -84,20 +86,26 @@ if __name__ == "__main__":
 
         dG_FEP[i,:]  = -np.log(np.sum(np.exp(alpha*Eij[bin_frames,:]),axis=0)/n_frames)
         
+        print "Bin %4d" % i
         for k in range(1,5):
             for j in range(Eij.shape[1]):
                 dG_cumulant_exp[k - 1,i,j] = ((alpha**k)/gamma(k + 1))*kstat(Eij[bin_frames,j],n=k)
 
 
+    if not os.path.exists("FEPestimate"):
+        os.mkdir("FEPestimate")
+    os.chdir("FEPestimate")
+
+
+    for i in range(4):
+        np.save("cumulant_exp_term_%d_vs_Q.npy" % i,dG_cumulant_exp[i,:,:])
+
+    raise SystemExit
+
     dG_fluct_sum = -np.sum(dG_cumulant_exp[1:,:,:],axis=0)
     dG_cumu_mean = -dG_cumulant_exp[0,:,:]
 
     dG_cumulant_sum = -np.sum(dG_cumulant_exp,axis=0)
-
-
-    if not os.path.exists("FEPestimate"):
-        os.mkdir("FEPestimate")
-    os.chdir("FEPestimate")
 
     dG_cumulant = np.array([ np.zeros((3,Eij.shape[1])) for x in range(4) ])
     for i in range(3):
@@ -115,9 +123,6 @@ if __name__ == "__main__":
     dG_cumulant_TS_avg = -dG_cumulant[0,1,:]
     dG_cumulant_N_avg  = -dG_cumulant[0,2,:]
     
-    np.savetxt("Qavg.dat",Qavg)
-    #for k in range(5):
-
 
     #plt.figure()
     #plt.plot(dG_FEP[0,:],dG_cumulant_sum[0,:],'r.')
@@ -128,7 +133,16 @@ if __name__ == "__main__":
     #plt.plot([0],[0],'g.',label="TS")
     #plt.plot([0],[0],'b.',label="N")
 
-    #plt.show()
+    #plt.figure()
+    #plt.plot(dG_FEP[0,:],-dG_cumulant[0,0,:]-dG_cumulant[1,0,:],'r.')
+    #plt.plot(dG_FEP[1,:],-dG_cumulant[0,1,:]-dG_cumulant[1,1,:],'g.')
+    #plt.plot(dG_FEP[2,:],-dG_cumulant[0,2,:]-dG_cumulant[1,2,:],'b.')
+
+    #plt.plot([0],[0],'r.',label="U")
+    #plt.plot([0],[0],'g.',label="TS")
+    #plt.plot([0],[0],'b.',label="N")
+
+    plt.show()
 
     raise SystemExit
 
@@ -214,8 +228,6 @@ if __name__ == "__main__":
     plt.savefig("TS_eps_fluct_relative.pdf")
     plt.savefig("TS_eps_fluct_relative.eps")
     
-
     #plt.show()
-
 
     os.chdir("../../..")

fluct.py
@@ -114,7 +114,6 @@ if __name__ == "__main__":
     name = args.name
     iteration = args.iteration
     n_bins = args.nbins
-    #name = "S6"
 
     #energy_entropy_fluctuations(name)
 
@@ -205,11 +204,13 @@ if __name__ == "__main__":
         dEdS[i] = np.mean(map(np.dot,dE,dS))
 
 
-
     baseline_fluct = dcontact_E[0,:] + dcontact_E[-1,:]
 
 
     relative_TS_fluct = (stdEij_TS**2)/(0.5*(stdEij_U**2 + stdEij_N**2))
+
+    ddG_vs_Q = -0.5*(dcontact_E - (stdEij_U**2)) - (contact_avgE - avgEij_U)
+    ddG_deriv_vs_Q = np.gradient(ddG_vs_Q,Qavg[1] - Qavg[0],Qavg[1] - Qavg[0])[0]
 
     if not os.path.exists("fluct"):
         os.mkdir("fluct")
@@ -218,12 +219,14 @@ if __name__ == "__main__":
     np.savetxt("Qavg.dat",Qavg)
     np.savetxt("dE2.dat",dE2)
     np.savetxt("dS2.dat",dS2)
+    np.savetxt("dEdS.dat",dEdS)
     np.savetxt("contact_Q.dat",contact_Q)
     np.savetxt("dQ2.dat",dQ2)
     np.savetxt("contact_avgE.dat",contact_avgE)
     np.savetxt("dcontact_E.dat",dcontact_E)
     np.savetxt("dcontact_S.dat",dcontact_E)
-    #np.savetxt("")
+    np.savetxt("ddG_vs_Q.dat",ddG_vs_Q)
+    np.savetxt("ddG_deriv_vs_Q.dat",ddG_deriv_vs_Q)
 
     Y = dcontact_E/(0.5*(stdEij_U**2 + stdEij_N**2))
 
@@ -254,13 +257,23 @@ if __name__ == "__main__":
 
     plt.figure()
     for i in range(Eij.shape[1]):
-        plt.plot(Qavg,0.5*(dcontact_E[:,i] - (stdEij_U[i]**2)) - (contact_avgE[:,i] - avgEij_U[i]),color=cm.gnuplot2((relative_TS_fluct[i] - relative_TS_fluct.min())/relative_TS_fluct.max()))
+        plt.plot(Qavg,ddG_vs_Q[:,i],color=cm.gnuplot2((relative_TS_fluct[i] - relative_TS_fluct.min())/relative_TS_fluct.max()))
     plt.xlabel("$Q$",fontsize=20)
-    plt.ylabel("$\\Delta\\Delta G = \\frac{\\Delta\\sigma^2_{E_i}}{2} - \\Delta\\overline{E}_i$",fontsize=16)
+    plt.ylabel("$\\Delta\\Delta G = -\\frac{\\Delta\\sigma^2_{E_i}}{2} - \\Delta\\overline{E}_i$",fontsize=16)
     plt.title("$\\Delta\\Delta G$ model with fluctuations",fontsize=18)
     plt.savefig("ddG_vs_Q_with_fluctuations.png")
     plt.savefig("ddG_vs_Q_with_fluctuations.pdf")
     plt.savefig("ddG_vs_Q_with_fluctuations.eps")
+
+    plt.figure()
+    for i in range(Eij.shape[1]):
+        plt.plot(Qavg,ddG_deriv_vs_Q[:,i],color=cm.gnuplot2((relative_TS_fluct[i] - relative_TS_fluct.min())/relative_TS_fluct.max()))
+    plt.xlabel("$Q$",fontsize=20)
+    plt.ylabel("$\\frac{d\\Delta\\Delta G}{dQ}$",fontsize=16,rotation="horizontal")
+    plt.title("$\\Delta\\Delta G$ model with fluctuations",fontsize=18)
+    plt.savefig("ddG_deriv_vs_Q.png")
+    plt.savefig("ddG_deriv_vs_Q.pdf")
+    plt.savefig("ddG_deriv_vs_Q.eps")
 
     plt.figure()
     for i in range(Eij.shape[1]):
@@ -307,8 +320,6 @@ if __name__ == "__main__":
     plt.savefig("TS_eps_fluct_relative.pdf")
     plt.savefig("TS_eps_fluct_relative.eps")
     
-
-    #plt.show()
-
+    plt.show()
 
     os.chdir("../../..")