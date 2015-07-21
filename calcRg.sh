13080f15475d730ce99c061e1a4dde59b93dbf02
Alexander Kluber <ajkluber@rice.edu>
2015-07-21 17:12:26 -0500

    [calcRg.sh] Delete gyrate.xvg from gromacs afterwards

--- calcRg.sh
+++ calcRg.sh
@@ -6,5 +6,6 @@ for x in ${dirs}; do
     echo "${x}" 
     echo "0" | g_gyrate_sbm -f traj.xtc -s topol_4.5.tpr -xvg none &> rg.dat
     awk '{print $2}' gyrate.xvg > Rg.dat
+    rm gyrate.xvg
     cd ../
 done
