8dccecf4363a2c47b52a90bd71eb82abf9719e21
Alexander Kluber <ajkluber@rice.edu>
2015-07-23 14:20:32 -0500

    [calcA.sh] Typo in sed command

--- calcA.sh
+++ calcA.sh
@@ -9,7 +9,7 @@ for x in ${dirs}; do
     n_nat=$(expr $temp - 1)
     n_nonnat=`cat pairwise_params | sed "1d" | awk '{print $1, $2, 0.15}' | uniq | sed "${n_nat}d" | wc -l`
     echo "${n_nonnat}" > nonnative_contacts.dat
-    cat pairwise_params | sed "1d" | awk '{print $1, $2, 0.5}' | uniq | sed "${n_nat}d" >> nonnative_contacts.dat
+    cat pairwise_params | sed "1d" | awk '{print $1, $2, 0.5}' | uniq | sed "1,${n_nat}d" >> nonnative_contacts.dat
 
     # Calculate total number of non-native contacts
     g_kuh_sbm -s Native.pdb -f traj.xtc -nc nonnative_contacts.dat -o A -noshortcut -abscut -cut 0.15
