4846635707e29435d5d71bc0c183576308ca1361
Alexander Kluber <ajkluber@rice.edu>
2015-07-29 18:33:25 -0500

    [sim_quenching.py] Shorten quenching period

--- sim_quenching.py
+++ sim_quenching.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     Tf = float(open("Tf","r").read().rstrip("\n"))
 
     Tlist = [2*Tf, 2*Tf, 0.1*Tf, 0.1*Tf]
-    pslist = [0, 150, 100, 200]
+    pslist = [0, 200, 50, 50]
 
     model,fitopts = mdb.inputs.load_model(name)
 
